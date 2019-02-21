8f9469eb4f90a9f7273bb2b52104851ce79c8213
Vinayak <vinayak@zoomtail.com>
2019-02-22 00:09:41 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://todos:todos@13.232.78.78:27017/todos',
     // localUrl: 'mongodb://localhost/meanstacktutorials'
     localUrl: 'mongodb://localhost:27017/books'
 };
